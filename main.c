ed34958b07924e77084d399e270573bb50f322fd
Noel Nicolas <c15noel.nicolas@usafa.edu>
2013-12-04 20:53:57 -0700

    right sensor, lights up LED2

--- main.c
+++ main.c
@@ -9,17 +9,18 @@ int main(void)
   ADC10CTL1 = INCH_3;                       // input A4
   ADC10AE0 |= BIT3;                         // PA.1 ADC option select
   ADC10CTL1 |= ADC10SSEL1|ADC10SSEL0;                // Select SMCLK
-  P1DIR |= 0x01;                            // Set P1.0 to output direction
+  P1DIR |= 0x41;                            // Set P1.0 to output direction
 
   for (;;)
   {
     ADC10CTL0 |= ENC + ADC10SC;             // Sampling and conversion start
     __bis_SR_register(CPUOFF + GIE);        // LPM0, ADC10_ISR will force exit
     if (ADC10MEM < 0x2FF)
-      P1OUT &= ~0x01;                       // Clear P1.0 LED off
+      P1OUT &= ~0x41;                       // Clear P1.0 LED off
 
     else
-      P1OUT |= 0x01;                        // Set P1.0 LED on
+      P1OUT |= BIT6;                        // Set P1.0 LED on
+
   }
 
     return 0;
